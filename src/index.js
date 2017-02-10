f57bfbd0a41e2885493ea8e580ba07df1d28432b
ironpup <christoph.ding@gmail.com>
2017-02-10 11:54:19 -0500

    Configure webpack to translate react and jsx.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
-console.log('hello!');
+class TestApp extends React.Component{
+  render() {
+    return (
+      <h1 className="header center teal-text text-lighten-2"> Heya </h1>
+    )
+  }
+}
+
+render(<TestApp/>, document.getElementById('mount-point'));
+
