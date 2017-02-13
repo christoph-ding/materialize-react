c532237504f0a7396eb3b88b31dc241f77e9d15f
ironpup <christoph.ding@gmail.com>
2017-02-13 13:49:58 -0500

    Refactoring the test app to handle many different nested components.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
+
+// MUI components
+import RaisedButton from 'material-ui/RaisedButton';
 
 class TestApp extends React.Component{
   render() {
     return (
+      <div>
       <MuiThemeProvider>
-        <RaisedButton label="click me"/>
-      </MuiThemeProvider>
+        <Button />
+      </MuiThemeProvider>        
+      </div>
+    )
+  }
+}
+
+class Button extends React.Component{
+  render() {
+    return (
+        <RaisedButton label="Raised Button"/>
     )
   }
 }
 
 render(<TestApp/>, document.getElementById('mount-point'));
-
